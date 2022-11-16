bf7420544d1261d01fb753720c2027f514685322
regentmango7 <regentmango7@gmail.com>
2022-11-16 22:37:30 +0000

    NEEDS getTrajectory() UPDATED TO WORK WITH T_des pointer and q_des pointer

--- src/node.cpp
+++ src/node.cpp
@@ -44,11 +44,11 @@ void start_competition(ros::NodeHandle & node){
   // If it's not already ready, wait for it to be ready.
   // Calling the Service using the client before the server is ready would fail.
   if (!begin_client.exists()) {
-    ROS_INFO("Waiting for the competition to be ready...");
+    ROS_WARN("Waiting for the competition to be ready...");
     begin_client.waitForExistence();
-    ROS_INFO("Competition is now ready.");
+    ROS_WARN("Competition is now ready.");
   }
-  ROS_INFO("Requesting competition start...");
+  ROS_WARN("Requesting competition start...");
   std_srvs::Trigger begin_comp;  // Combination of the "request" and the "response".
   bool service_call_succeeded;
   service_call_succeeded =  begin_client.call(begin_comp);  // Call the service to start the competition. Returns true if the client was able to initiate the service
@@ -57,13 +57,13 @@ void start_competition(ros::NodeHandle & node){
   } else if (!begin_comp.response.success) {  // Even if the call to the service succeeded, the service might not execute successfully. If execution failed, print why.
       ROS_WARN("Competition service returned failure: %s", begin_comp.response.message.c_str());
   } else { // if the service was called and executed successfully, inform user
-      ROS_INFO("Competition service called succesfully! %s", begin_comp.response.message.c_str());
+      ROS_WARN("Competition service called succesfully! %s", begin_comp.response.message.c_str());
   }
 }
  
 
 void order_callback(const osrf_gear::Order::ConstPtr & order_msg) {
-  ROS_INFO_STREAM("Received order:\n" << *order_msg);  // Display incoming order messages, which contain a unit_ids (string) and lists of shipments (vector).  
+  ROS_WARN_STREAM("Received order:\n" << *order_msg);  // Display incoming order messages, which contain a unit_ids (string) and lists of shipments (vector).  
   received_orders.push_back(*order_msg); // Add new orders to the end of the list of orders
 }
 
@@ -125,18 +125,18 @@ void joint_state_callback(const sensor_msgs::JointState::ConstPtr & msg){
 }
 
 // Method for identifying the name of the bin that a given product is in.
-std::string find_bin(std::string material_type, ros::ServiceClient client){
+std::string find_bin(std::string material_type, ros::ServiceClient *client){
   osrf_gear::GetMaterialLocations location_msg; // location_msg can contain a material_type string and list of storage units where the material may be found
   location_msg.request.material_type = material_type;
   bool location_call_succeeded;
-  location_call_succeeded = client.call(location_msg); // query the material location
+  location_call_succeeded = client->call(location_msg); // query the material location
   if (!location_call_succeeded){
     ROS_WARN("Material location returned failure.");
   } else{ // If the query succeeded, iterate through the list of storage units and return the name of the first storage unit containing the given material type that is not the belt.
       
     for (osrf_gear::StorageUnit unit : location_msg.response.storage_units){
       if(unit.unit_id != "belt"){
-      	ROS_INFO("%s is found in storage unit %s", material_type.c_str(), unit.unit_id.c_str());
+      	ROS_WARN("%s is found in storage unit %s", material_type.c_str(), unit.unit_id.c_str());
      	  	return unit.unit_id.c_str();
         }
         
@@ -145,26 +145,30 @@ std::string find_bin(std::string material_type, ros::ServiceClient client){
   return "NONE"; // if no storage units contain the given material type, return "NONE"
 }
 
-int getTrajectory(trajectory_msgs::JointTrajectory *p_joint_trajectory, geometry_msgs::PoseStamped goal_pose){
-	double T_des[4][4];
-	double q_des[8][6];
+int getTrajectory(trajectory_msgs::JointTrajectory *p_joint_trajectory, geometry_msgs::PoseStamped *goal_pose, double *p_T_des, double *p_q_des){
+
+	//THIS NEEDS FIXED
 	// Desired pose of the end effector wrt the base_link.
-	T_des[0][3] = goal_pose.pose.position.x;
-	T_des[1][3] = goal_pose.pose.position.y;
-	T_des[2][3] = goal_pose.pose.position.z + 0.3; // above part
-	T_des[3][3] = 1.0;
+	p_T_des[0][3] = goal_pose->pose.position.x;
+	p_T_des[1][3] = goal_pose->pose.position.y;
+	p_T_des[2][3] = goal_pose->pose.position.z + 0.3; // above part
+	p_T_des[3][3] = 1.0;
 	// The orientation of the end effector so that the end effector is down.
-	T_des[0][0] = 0.0; T_des[0][1] = -1.0; T_des[0][2] = 0.0;
-	T_des[1][0] = 0.0; T_des[1][1] = 0.0; T_des[1][2] = 1.0;
-	T_des[2][0] = -1.0; T_des[2][1] = 0.0; T_des[2][2] = 0.0;
-	T_des[3][0] = 0.0;  T_des[3][1] = 0.0; T_des[3][2] = 0.0;
-			
-	int num_sols = ur_kinematics::inverse((double *)&T_des, (double *)&q_des);
+	*p_T_des = 0.0; p_T_des[0][1] = -1.0; p_T_des[0][2] = 0.0;
+	p_T_des[1][0] = 0.0; p_T_des[1][1] = 0.0; p_T_des[1][2]  = 1.0;
+	p_T_des[2][0] = -1.0; p_T_des[2][1] = 0.0; p_T_des[2][2] = 0.0;
+	p_T_des[3][0] = 0.0;  p_T_des[3][1] = 0.0; p_T_des[3][2] = 0.0;
+	
+	int num_sols = ur_kinematics::inverse((double *)&p_T_des, (double *)&p_q_des);
+	
 	// Fill out the joint trajectory header.
 	// Each joint trajectory should have an non-monotonically increasing sequence number.
 	p_joint_trajectory->header.seq = count++;
 	p_joint_trajectory->header.stamp = ros::Time::now(); // When was this message created.
 	p_joint_trajectory->header.frame_id = "/world"; // Frame in which this is specified.
+	
+	
+
 				
 	// Set the names of the joints being used.  All must be present.
 	p_joint_trajectory->joint_names.clear();
@@ -176,6 +180,7 @@ int getTrajectory(trajectory_msgs::JointTrajectory *p_joint_trajectory, geometry
 	p_joint_trajectory->joint_names.push_back("wrist_2_joint");
 	p_joint_trajectory->joint_names.push_back("wrist_3_joint");
 				
+
 	// Set a start and end point.
 	p_joint_trajectory->points.resize(2);
 	// Set the start point to the current position of the joints from joint_states.
@@ -188,22 +193,21 @@ int getTrajectory(trajectory_msgs::JointTrajectory *p_joint_trajectory, geometry
 			}
 		}
 	}
-	for(int indv = 0; indv < p_joint_trajectory->joint_names.size(); indv++){
-		p_joint_trajectory->points[0].velocities[indv] = 0;
-	}
 	// When to start (immediately upon receipt).
 	p_joint_trajectory->points[0].time_from_start = ros::Duration(0.0);
-			
+	
 	// Must select which of the num_sols solutions to use.  Just start with the first.
 	int q_des_indx = 0;
 	// Set the end point for the movement
 	p_joint_trajectory->points[1].positions.resize(p_joint_trajectory->joint_names.size());
 	// Set the linear_arm_actuator_joint from joint_states as it is not part of the inversen kinematics solution.
 	p_joint_trajectory->points[1].positions[0] = joint_states.position[1];
+
 	// The actuators are commanded in an odd order, enter the joint positions in the correct positions
 	for (int indy = 0; indy < 6; indy++) {
-		p_joint_trajectory->points[1].positions[indy + 1] = q_des[q_des_indx][indy];
+		p_joint_trajectory->points[1].positions[indy + 1] = //THIS NEEDS FIXED p_q_des[q_des_indx][indy];
 	}
+
 	// How long to take for the movement.
 	p_joint_trajectory->points[1].time_from_start = ros::Duration(1.0);
 	return 0;
@@ -252,6 +256,7 @@ int main(int argc, char **argv)
    * buffer up before throwing some away.
    */
    
+   received_orders.clear();
    tf2_ros::Buffer tfBuffer;
    tf2_ros::TransformListener tfListener(tfBuffer);
 
@@ -271,9 +276,13 @@ int main(int argc, char **argv)
 	ros::Subscriber joint_state_sub = n.subscribe("/ariac/arm1/joint_states", 10, joint_state_callback);
 	
 	// Instantiate variables for use with the kinematic system.
-	double T_pose[4][4], T_des[4][4];
-	double q_pose[6], q_des[8][6];
+	double T_pose[4][4];
+	double q_pose[6];
+	double T_des[4][4];
+	double q_des[8][6];
+	geometry_msgs::PoseStamped part_pose, goal_pose;
 	trajectory_msgs::JointTrajectory desired;
+	trajectory_msgs::JointTrajectory trajectory;
 	
   //Subscribe to the '/ariac/orders' topic.
   ros::Subscriber orders_subscriber = n.subscribe("/ariac/orders", 10, order_callback);
@@ -292,17 +301,15 @@ int main(int argc, char **argv)
   spinner.start();
   while (ros::ok())
   {
-
 		// if there are active orders, find where the parts are
 		if(!received_orders.empty()){
 		  // store the first order in received_orders as a separate variable, then remove that order from the list. 
-			osrf_gear::Order order = received_orders.at(0); 
-			received_orders.erase(received_orders.begin()); 
+			osrf_gear::Order order = received_orders.at(0);
       // Each order contains a set of shipments, which in turn have a list of products. For all shipments in the current order, identify the bin that each product is in. 
       // Print the bin number and coordinates of the selected product in the bin. Track how many of that product type have been ordered (for inventory purposes).
 			for(osrf_gear::Shipment shipment: order.shipments){
 				for(osrf_gear::Product product:shipment.products){
-					std::string bin = find_bin(product.type.c_str(), location_client);
+					std::string bin = find_bin(product.type.c_str(), &location_client);
 						
 					if(mat_bin.find(product.type.c_str()) != mat_bin.end()){
 						mat_bin.at(product.type.c_str()).second += 1;
@@ -314,7 +321,7 @@ int main(int argc, char **argv)
 					// ROS_WARN_STREAM(product.type.c_str() << " is in bin: " << bin << " at position: x " << position.x << ", y " << position.y << ", z " << position.z);
 					
 					
-				//START CONVERSION OF PART POSITION RELATIVE TO ARM
+					//START CONVERSION OF PART POSITION RELATIVE TO ARM
 					// find camera frame
 					std::string camera_frame_name = "logical_camera_" + bin + "_frame";
 					
@@ -329,7 +336,7 @@ int main(int argc, char **argv)
 						ROS_ERROR("%s", ex.what());
 					}
 					
-					geometry_msgs::PoseStamped part_pose, goal_pose;
+					
 					
 					part_pose.pose = pose;
 					
@@ -343,22 +350,23 @@ int main(int argc, char **argv)
 					goal_pose.pose.orientation.y = 0.707;
 					goal_pose.pose.orientation.z = 0.0;
 					
-					ROS_WARN_STREAM(goal_pose);
+					ROS_WARN_STREAM("GOAL POSE: " << goal_pose);
 					
 					//START PROCESSING JOINT TRAJECTORY MESSAGE CONSTRUCTION
-					q_pose[0] = joint_states.position[1];
+					/*q_pose[0] = joint_states.position[1];
 					q_pose[1] = joint_states.position[2];
 					q_pose[2] = joint_states.position[3];
 					q_pose[3] = joint_states.position[4];
 					q_pose[4] = joint_states.position[5];
 					q_pose[5] = joint_states.position[6];
-					
+					*/
 					//ur_kinematics::forward((float *)&q_pose, (double *)&T_pose);
-					trajectory_msgs::JointTrajectory trajectory;
 					
-					int status = getTrajectory(&trajectory, goal_pose);
 					
-					//ROS_WARN_STREAM(trajectory);
+					
+					int status = getTrajectory(&trajectory, &goal_pose, &T_des[0][0], &q_des[0][0]);
+					
+					ROS_WARN_STREAM("TRAJECTORY: " << trajectory);
 					
 					
 					// Create the structure to populate for running the Action Server.
@@ -370,7 +378,7 @@ int main(int argc, char **argv)
 					joint_trajectory_as.action_goal.goal_id.id = count;
 					
 					actionlib::SimpleClientGoalState state = trajectory_as.sendGoalAndWait(joint_trajectory_as.action_goal.goal, ros::Duration(30.0), ros::Duration(30.0));
-					ROS_INFO("Action Server returned with status: [%i] %s", state.state_, state.toString().c_str());
+					ROS_WARN("Action Server returned with status: [%i] %s", state.state_, state.toString().c_str());
 					/*if(state.state_ != state.SUCCESS){
 						
 						
@@ -378,9 +386,10 @@ int main(int argc, char **argv)
 				
 				}
 			}
+			received_orders.erase(received_orders.begin()); 
 		}
 		
-		ROS_INFO_STREAM_THROTTLE(10, joint_states);
+		ROS_WARN_STREAM_THROTTLE(10, joint_states);
 		
 	  loop_rate.sleep();
 
